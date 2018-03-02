ce953e2cf14c897d2e9b085049cabf377680e660
Killian Perea Ruiz <killian.perea@worldline.com>
2018-03-02 10:06:24 +0100

    move cutom libs on a separete folder

--- generators/app/templates/src/main/resources/META-INF/resources/js/app/app.module.ts
+++ generators/app/templates/src/main/resources/META-INF/resources/js/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule,BrowserAnimationsModule } from './animations/src/animations';
+import { NoopAnimationsModule,BrowserAnimationsModule } from '../../libs/animations/src/animations';
 import 'chart.js/dist/Chart.min'
 
 
