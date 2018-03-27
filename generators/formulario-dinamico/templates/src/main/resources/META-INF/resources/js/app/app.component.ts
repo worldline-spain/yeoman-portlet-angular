af067a6b1056fac47714d5842e03fbf94610ac72
Killian Perea Ruiz <killian.perea@worldline.com>
2018-03-27 14:05:55 +0200

    formulario dinamico generator

---
+++ generators/formulario-dinamico/templates/src/main/resources/META-INF/resources/js/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { TranslateService, LiferayService } from '../../services/shared.module';
+
+
+@Component({
+	selector: 'app',
+	templateUrl: '/o/formulario-dinamico/js/app/app.html'
+})
+export class AppComponent {
+
+	caption = 'Hello world!';
+	values: string[];
+
+	constructor(
+		private translate: TranslateService,
+		private liferayService: LiferayService
+	) {
+		this.initTranslate();
+	}
+
+	initTranslate() {
+		// Set the default language for translation strings, and the current language.
+		this.translate.setDefaultLang(this.liferayService.getLanguageLiferay());
+
+		// Set your language here
+		this.translate.use(this.liferayService.getLanguageLiferay());
+	}
+}
