a2570a0ad7f545d456855a67a40960803626b9c6
Juan Belmonte Rodríguez <juan.belmonte@worldline.com>
2018-04-09 09:31:39 +0200

    feat(tests): Go back to PhantomJS browser and single run.

--- generators/test-karma/templates/karma.conf.js
+++ generators/test-karma/templates/karma.conf.js
@@ -74,9 +74,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-    // browsers: ['PhantomJS'],
-    // singleRun: true
+    // browsers: ['Chrome'],
+    // singleRun: false
+    browsers: ['PhantomJS'],
+    singleRun: true
   });
 };
